3a2799a4a429f720a3c98461d5fe810a927ba79c
alves <liulikeqian@gmail.com>
2025-12-30 14:37:04 +0800

    feature remove not work code.

--- src/slic3r/GUI/HttpServer.cpp
+++ src/slic3r/GUI/HttpServer.cpp
@@ -676,7 +676,7 @@ std::shared_ptr<HttpServer::Response> HttpServer::bbl_auth_handle_request(const 
             j["data"]["user"]["avatar"]     = user_avatar;
             agent->change_user(j.dump());
             if (agent->is_user_login()) {
-                wxGetApp().request_user_login(1);
+                //wxGetApp().request_user_login(1);
             }
             GUI::wxGetApp().CallAfter([] { wxGetApp().ShowUserLogin(false); });
             std::string location_str = (boost::format("%1%?result=success") % redirect_url).str();
